25f037d1b7a5ffac6f77f0439c66476a876456c1
Kacper Błażejewski <siarxxpl@gmail.com>
2020-06-13 14:13:50 +0200

    Shopcontroller single test

---
+++ GroceryDelivery.Tests/Controllers/ShopControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Mvc;
+using GroceryDelivery.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GroceryDelivery.Tests.Controllers
+{
+    [TestClass]
+    public class ShopControllerTest
+    {
+        [TestMethod]
+        public void TestSumUmpRedirectToAction()
+        {
+            // Arange
+
+            ShopController controller = new ShopController();
+
+            // Act
+
+            var result = controller.SumUp() as RedirectToRouteResult;
+
+            // Assert
+
+            Assert.AreEqual("Index", result.RouteValues["action"]);
+            Assert.AreEqual("Home", result.RouteValues["controller"]);
+        }
+    }
+}
